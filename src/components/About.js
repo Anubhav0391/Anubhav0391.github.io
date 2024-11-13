d8466ae8b766b0b8824ce869d97a02ebb8d65b6b
Anubhav0391 <anubhavdixit268@gmail.com>
2024-11-13 19:44:55 +0530

    version-1.0

---
+++ src/components/About.js
@@ -0,0 +1,30 @@
+
+
+
+const profile_picture =       ""
+
+
+
+const name = "Anubhav Dixit";
+
+const tech_stack = [
+    "React.JS", "JavaScript", "TypeScript",
+    "Redux", "Redux Toolkit", "RTK Query",
+    "CSS", "Material UI", "Tailwind", "Shadcn",
+    "Jest", "React Testing Library",
+    "CI/CD - Github Actions"
+];
+
+function contact_me () {
+    if (have_an_opening || have_a_project) {
+        navigate('/contact')
+    }
+}
+
+
+
+
+
+
+
+
